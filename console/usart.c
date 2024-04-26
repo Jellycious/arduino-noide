434701aad80231ac9caa8d6da7bad8b3c093bc43
Jelle Meijer <j.meijer@sst.nl>
2024-04-26 13:08:39 +0200

    add usart support for different baud rates

--- console/usart.c
+++ console/usart.c
@@ -2,19 +2,88 @@
 
 #include "usart.h"
 
-void usart_init() {
-    /* Set baud rate */
+static void usart_4800() {
+    #undef BAUD
+    #define BAUD 4800
+    #include <util/setbaud.h>
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    /* Enable Transmitter and Receiver */
+    UCSR0B = (1 << RXEN0)|(1 << TXEN0);
+    return;
+}
+
+static void usart_9600() {
+    #undef BAUD
     #define BAUD 9600
     #include <util/setbaud.h>
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
-    
-    // Enable transmitter and receiver
+    /* Enable Transmitter and Receiver */
     UCSR0B = (1 << RXEN0)|(1 << TXEN0);
+    return;
+}
+
+static void usart_19200() {
+    #undef BAUD
+    #define BAUD 19200
+    #include <util/setbaud.h>
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    /* Enable Transmitter and Receiver */
+    UCSR0B = (1 << RXEN0)|(1 << TXEN0);
+    return;
+}
+
+static void usart_38400() {
+    #undef BAUD
+    #define BAUD 38400
+    #include <util/setbaud.h>
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    /* Enable Transmitter and Receiver */
+    UCSR0B = (1 << RXEN0)|(1 << TXEN0);
+    return;
+}
+
+static void usart_57600() {
+    #undef BAUD
+    #define BAUD 57600
+    #include <util/setbaud.h>
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    /* Enable Transmitter and Receiver */
+    UCSR0B = (1 << RXEN0)|(1 << TXEN0);
+    return;
+}
+
+void usart_init(USART_BAUD_t baud) {
+
+    /* Set baud rate */
+    switch(baud) {
+        case USART_BAUD_4800:
+            usart_4800();
+            break;
+        case USART_BAUD_9600:
+            usart_9600();
+            break;
+        case USART_BAUD_19200:
+            usart_19200();
+            break;
+        case USART_BAUD_38400:
+            usart_38400();
+            break;
+        case USART_BAUD_57600:
+            usart_57600();
+            break;
+        default:
+            break;
+    }
 }
 
 int usart_putchar(char c, FILE *stream) {
     if (c == '\n') {
+        /* Insert extra carriage return before new line */
         usart_putchar('\r', stream);
     }
     loop_until_bit_is_set(UCSR0A, UDRE0);
--- console/usart.h
+++ console/usart.h
@@ -4,16 +4,26 @@
 #ifndef _USART_H
 #define _USART_H
 
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void usart_init();
-void usart_9600();
+typedef enum {
+    USART_BAUD_4800,
+    USART_BAUD_9600,
+    USART_BAUD_19200,
+    USART_BAUD_38400,
+    USART_BAUD_57600,
+    USART_BAUD_115200,
+} USART_BAUD_t;
+
+void usart_init(USART_BAUD_t baud);
 int usart_putchar(char c, FILE *stream);
 int usart_getchar(FILE *stream);
 
-#ifdef __cpluspluc
+#ifdef __cplusplus
 }
 #endif /* END __cplusplus */
 
